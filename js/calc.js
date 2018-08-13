43c95cc8934e936f625786c9c105badf816ee28f
mcarreon5 <milliecarreon5@gmail.com>
2018-08-13 16:58:07 +0000

    Done.

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,37 @@
 //add your code here
+$(document).ready(function(){
+  doMath();
+  //result();
+});
+var num1 = $('#number1');
+var num2 = $('#number2');
+var operator = $('#operation');
+var total = $('#result');
+
+
+function doMath(){
+  $('#equals').on('click', function(){
+    num1 = parseFloat(num1.val());
+    num2 = parseFloat(num2.val());
+    console.log(num1)
+    operator = operator.val();
+	  result(operator,num1,num2);
+  });
+}
+
+function result(operator,num1,num2){
+  $('#equals').on('click', function(){
+    let turkey;
+    if (operator === "+") {
+       turkey = num1 + num2;
+    } else if (operator === "-") {
+       turkey = num1 - num2;
+    } else if (operator === "*") {
+       turkey = num1 * num2;
+    } else if (operator === "/") {
+      turkey = num1 / num2;
+    }
+    });
+  $("#result").val(turkey);
+}
+  
