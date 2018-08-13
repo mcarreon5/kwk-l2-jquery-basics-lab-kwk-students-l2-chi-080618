ade287685fa5da427fa72209958eadc702efa905
mcarreon5 <milliecarreon5@gmail.com>
2018-08-13 16:05:50 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -1,23 +1,25 @@
 //add your code here
 $(document).ready(function(){
   doMath();
+  result();
 });
 var num1 = $('#number1');
 var num2 = $('#number2');
 var operator = $('#operation');
 var result = $('#result');
 
-function result(operator,num1,num2){
-  let turkey = num1 + num2;
-  console.log(turkey)
-	return result.val(turkey)
-}
 
 function doMath(){
   $('#equals').on('click', function(){
     num1 = parseFloat(num1.val());
     num2 = parseFloat(num2.val());
     operator = operator.val();
-	  result(operator,num1,num2);
+	  //result(operator,num1,num2);
   });
+  
+function result(operator,num1,num2){
+  let turkey = num1 + num2;
+  console.log(turkey)
+	return result.val(turkey)
+  }
 }
