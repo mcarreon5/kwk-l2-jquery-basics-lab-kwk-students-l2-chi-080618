1607db5c2fa375f37a7c99e5b6c16eb5d0297bf8
mcarreon5 <milliecarreon5@gmail.com>
2018-08-13 15:30:52 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
   domath();
 });
 var num1 = $('#number1');
+var num2 = $('#number2');
+var operator = $('#operation');
+
+function doMath(){
+  $('#equals').on('click', function(){
+    num = parseFloat(num1.val());
+  }
+}
